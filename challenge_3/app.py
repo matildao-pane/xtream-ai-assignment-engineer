274414333b4cbe6366a46b1dc686eee7ef3b51fb
matildao-pane <edlitam@hotmail.it>
2024-04-28 22:56:19 +0200

    feat(challenge_3): integrate model, adjust css

--- challenge_3/app.py
+++ challenge_3/app.py
@@ -1,13 +1,14 @@
 import pickle
-import joblib
 import pandas as pd
 from flask import Flask, request, jsonify,render_template
 import os
+import numpy as np
 
 app = Flask(__name__)
 
 # Load the trained model
-#model = joblib.load(os.getcwd()+'\challenge_3\XGBoost_.pkl')
+with open("./challenge_3/xgboost_.pkl", "rb") as file:
+    model = pickle.load(file)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,15 +16,16 @@ def index():
         # Get the input data from the form
         data = request.form.to_dict()
         print(data)
-        #depth = float(data['depth'])
-        #table = float(data['table'])
-        #cut = int(data['cut'])
-        #color = int(data['color'])
-        #clarity = int(data['clarity'])
-        #carat = float(data['carat'])
-        #price = model.predict([[cut]])[0]
-        price = 44444
-        return jsonify({'price': price})
+        depth = float(data['depth'])
+        table = float(data['table'])
+        cut = int(data['cut'])
+        x = float(data['x'])
+        color = int(data['color'])
+        clarity = int(data['clarity'])
+        carat = float(data['carat'])
+        price = model.predict([[carat,cut,color,clarity,depth,table,x]])[0]
+        exp_price= np.expm1(price)
+        return jsonify({'price': float(exp_price)})
     return render_template('index.html')
  
 if __name__ == '__main__':
